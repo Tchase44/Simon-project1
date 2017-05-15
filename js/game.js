df802fe04a19d45ea96d4d804fd5def817a15c6b
Tim Chase <tchase4544@gmail.com>
2017-05-15 10:48:56 -0400

    track user touch, populate computer order

--- js/game.js
+++ js/game.js
@@ -5,6 +5,40 @@
 
 $(document).ready(function(){
 	let userTouch = [];
-	
+	const comp = {
+		easyIndex : [],
+		hardIndex :[]
+	}
+	const colorEasy = ['green','red','blue','yellow']
+
+	$('.touch').on("click",function(){
+		populate()
+		userTouch.push($(this).attr("id"))
+		// console.log(`cpu ${comp.easyIndex}`)
+		console.log(userTouch)
+	})
+
+
+	function populate() {
+		let selection = randomEasy();
+		comp.easyIndex.push(colorEasy[selection])
+	}
+
+	//returns random number fo easy
+	const randomEasy = () => Math.floor(Math.random()*4)
+
+/*
+comp array based off random numbers
+
+display num/color to user
+
+userTouch recorded && compared
+
+must be exact match
+
+keep track of high scores
+
+*/
+//Bounus = Hard Mode
 
 });
